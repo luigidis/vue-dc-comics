343fb82ba8cbc666700e5e19cc6d691067225bf5
LuigiDiS <luigi.distefano69@gmail.com>
2022-09-06 16:00:01 +0200

    Layout e ciclo v-for MainContent

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,22 +1,53 @@
 <template>
     <div class="bg-color-black">
-        ---> Content goes here 
+       <div class="container">
+            <div class="card" v-for="(banner,i) in banners" :key="i">
+                <img :src="banner.thumb" :alt=banner.type>
+                <h3>{{ banner.series}}</h3>
+            </div>
+        </div>
+
     </div>
 </template>
 <script>
-export default {
-
-}
+    import banners from '../banners'
+    export default {
+        data() {
+            return { 
+                banners: banners
+            }
+        },
+        mounted() {
+             console.log(this.banners)
+        },
+    }   
 </script>
 
 <style lang="scss" scoped>
  .bg-color-black {
-    height: 160px;
+    height: 600px;
     background-color: #1C1C1C;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-size: 3rem;
- }
+    .container {
+        display: grid;
+        grid-template-columns: repeat(6,1fr);
+        gap: 30px;
+        .card {
+            padding: 10px;
+            display: flex;
+            flex-direction: column;
+            img {
+                width: 185px;
+                aspect-ratio: 1;
+                flex-basis: 80%;
+            }
+            h3 {
+                color: white;
+                text-transform: uppercase;
+            }
+        }
+            
+
+    }
+}
+    
 </style>
